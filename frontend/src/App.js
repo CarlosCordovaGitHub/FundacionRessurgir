2fee6841396a68b442e235198d2cab59c31fef67
Hernán Sánchez <herez2002@gmail.com>
2024-10-06 20:48:37 -0500

    Cambio navbar y Testimonios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Emprendimientos from './pages/Emprendimientos';
+import Testimonios from './pages/Testimonios';
+import Navbar from './components/Navbar'; // Asegúrate de que la ruta sea correcta
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/emprendimientos" element={<Emprendimientos />} />
-        {/* Otras rutas */}
-      </Routes>
+      <div>
+        {/* Coloca el Navbar aquí para que se vea en todas las páginas */}
+        <Navbar />
+
+        {/* Las rutas de tu aplicación */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/emprendimientos" element={<Emprendimientos />} />
+          <Route path="/testimonios" element={<Testimonios />} />
+          {/* Otras rutas */}
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
-
